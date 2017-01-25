2953ef3924e06e97fd5189803db2cebf150a8d23
tatianalooby <tatianalooby@gmail.com>
2017-01-25 05:19:17 +0300

    initial commit

---
+++ CustomPickerView/PrototypeCell.swift
@@ -0,0 +1,17 @@
+//
+//  prototypeCell.swift
+//  CustomPickerView
+//
+//  Created by Tatiana Looby on 24/01/2017.
+//  Copyright © 2017 Tatiana Looby. All rights reserved.
+//
+
+import UIKit
+
+class PrototypeCell: UITableViewCell {
+    
+    @IBOutlet weak var teachingGroupLabel: UILabel!
+    
+    @IBOutlet weak var roomLabel: UILabel!
+    
+}
